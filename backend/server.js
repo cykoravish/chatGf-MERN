c87fad09ae2156515d280b30aaa08165ac8a5378
Ravish Bisht <ravishbisht85@gmail.com>
2024-03-15 08:57:44 +0530

    signup add

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,15 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-dotenv.config();
-
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("hello world bro guys");
-});
-
+app.use(express.json())
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-  connectToMongoDB()
+  connectToMongoDB();
   console.log(`server is running at port ${PORT}`);
 });
