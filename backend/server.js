33a136abdd1c0c1b94871c90939af52ca83f4330
Ravish Bisht <ravishbisht85@gmail.com>
2024-03-15 15:14:37 +0530

    middleware add

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -8,8 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser()
+);
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
