cf53533aebf36f37a716d9467d0ec6905f1a46c6
Ravish Bisht <ravishbisht85@gmail.com>
2024-03-17 16:49:46 +0530

    home page sidebar add

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import SearchInput from "./SearchInput";
+import Conversations from "./Conversations";
+import LogoutButton from "./LogoutButton";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="border-r border-slate-500 p-4 flex flex-col">
+      <SearchInput />
+      <div className="divider px-3"></div>
+      <Conversations />
+      <LogoutButton />
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
